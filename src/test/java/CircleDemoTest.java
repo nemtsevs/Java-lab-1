871dd7a1118751bbc00b3b1c6b519788ac7d07a4
Stas Nemtsev <nemtsevs@list.ru>
2018-05-05 11:51:22 +0300

    New

--- src/test/java/CircleDemoTest.java
+++ src/test/java/CircleDemoTest.java
@@ -1,4 +1,4 @@
-import org.junit.After;
+﻿import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -24,50 +24,50 @@ public class CircleDemoTest
     @Test
     public void exercise1_1() {
         CircleDemo.exercise1(1,0.001);
-        assertEquals("Зазор равен 15,92 сантиметров. ", outStream.toString());
+        assertEquals("Зазор равен 15.92 сантиметров. ", outStream.toString());
     }
     @Test
     public void exercise1_100() {
         CircleDemo.exercise1(100,0.001);
-        assertEquals("Зазор равен 15,92 сантиметров. ", outStream.toString());
+        assertEquals("Зазор равен 15.92 сантиметров. ", outStream.toString());
     }
     @Test
     public void exercise1_1000() {
         CircleDemo.exercise1(1000,0.001);
-        assertEquals("Зазор равен 15,92 сантиметров. ", outStream.toString());
+        assertEquals("Зазор равен 15.92 сантиметров. ", outStream.toString());
     }
 
     @Test
     public void exercise1_1000_001() {
         CircleDemo.exercise1(1000,0.01);
-        assertEquals("Зазор равен 159,16 сантиметров. ", outStream.toString());
+        assertEquals("Зазор равен 159.16 сантиметров. ", outStream.toString());
     }
     @Test
     public void exercise1_1000_01() {
         CircleDemo.exercise1(1000,0.1);
-        assertEquals("Зазор равен 1591,55 сантиметров. ", outStream.toString());
+        assertEquals("Зазор равен 1591.55 сантиметров. ", outStream.toString());
     }
 
     @Test
     public void exercise1_6378_01() {
         CircleDemo.exercise1(6378.1,0.1);
-        assertEquals("Зазор равен 1591,55 сантиметров. ", outStream.toString());
+        assertEquals("Зазор равен 1591.55 сантиметров. ", outStream.toString());
     }
     @Test
     public void exercise1_6378_001() {
         CircleDemo.exercise1(6378.1,0.01);
-        assertEquals("Зазор равен 159,16 сантиметров. ", outStream.toString());
+        assertEquals("Зазор равен 159.16 сантиметров. ", outStream.toString());
     }
 
     @Test
     public void exercise1() {
         CircleDemo.exercise1(6378.1,0.001);
-        assertEquals("Зазор равен 15,92 сантиметров. ", outStream.toString());
+        assertEquals("Зазор равен 15.92 сантиметров. ", outStream.toString());
     }
 
     @Test
     public void exercise2() {
         CircleDemo.exercise2(3,1,1000,2000);
-        assertEquals("Стоимость дорожки и ограды равны 21991,13 и 50265,44 рублей.", outStream.toString());
+        assertEquals("Стоимость дорожки и ограды равны 21991.13 и 50265.44 рублей.", outStream.toString());
     }
 }
--- src/test/java/SqrtDemoTest.java
+++ src/test/java/SqrtDemoTest.java
@@ -24,24 +24,24 @@ public class SqrtDemoTest
     @Test
     public void printSqrt_2_15() {
         SqrtDemo.printSqrt(2,15);
-        assertEquals("Sqrt of 2.0 (delta = 1.0E-15) = 1,414213562373095.", outStream.toString());
+        assertEquals("Sqrt of 2.0 (delta = 1.0E-15) = 1.414213562373095.", outStream.toString());
     }
 
     @Test
     public void printSqrt_3_14() {
         SqrtDemo.printSqrt(3,14);
-        assertEquals("Sqrt of 3.0 (delta = 1.0E-14) = 1,73205080756888.", outStream.toString());
+        assertEquals("Sqrt of 3.0 (delta = 1.0E-14) = 1.73205080756888.", outStream.toString());
     }
 
     @Test
     public void printSqrt_5_13() {
         SqrtDemo.printSqrt(5,13);
-        assertEquals("Sqrt of 5.0 (delta = 1.0E-13) = 2,2360679774998.", outStream.toString());
+        assertEquals("Sqrt of 5.0 (delta = 1.0E-13) = 2.2360679774998.", outStream.toString());
     }
 
     @Test
     public void printSqrt_7_15() {
         SqrtDemo.printSqrt(7,15);
-        assertEquals("Sqrt of 7.0 (delta = 1.0E-15) = 2,645751311064591.", outStream.toString());
+        assertEquals("Sqrt of 7.0 (delta = 1.0E-15) = 2.645751311064591.", outStream.toString());
     }
 }
